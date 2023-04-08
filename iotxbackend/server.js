5cc7008a43d64ca74c8845b10da4ec2a9fb5e9f4
relieyanhilman <relieyanhilman93@gmail.com>
2023-04-09 00:57:34 +0700

    add api check-in and check-out users

--- iotxbackend/server.js
+++ iotxbackend/server.js
@@ -1,27 +1,207 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { authenticateUser } = require("./connectDB");
-
+const { pool } = require("./connectDB");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post("/send-data", (req, res) => {
+//endpoint buat tes bisa terhubung ke database atau tidak
+app.get("/", (req, res) => {
+  pool.query("SELECT NOW()", (err, res) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`PostgreSQL connected: ${res.rows[0].now}`);
+    }
+  });
+});
+
+//endpoint untuk login
+app.post("/checkIn", async (req, res) => {
+  //mengambil data dari request.body
   const data = req.body;
 
-  // Do something with the data here
-
-  //   console.log(data.plat_number);
-
   try {
-    // authenticateUser(data.plat_number);
-    console.log(authenticateUser(data.plat_number));
+    const authenticatedUser = await authenticateUserIn(data.plat_number);
+    res.json("check-in berhasil");
+    return authenticatedUser;
+    //fungsi kirim data ke database
   } catch (err) {
-    console.log(err);
-    res.send("failed to receive data");
+    res.json("check-in gagal");
+    return err;
   }
 });
+
+app.post("/checkOut", async (req, res) => {
+  //mengambil data dari request.body
+  const data = req.body;
+
+  try {
+    const authenticatedUser = await authenticateUserOut(data.plat_number);
+    const price_rent_parking = authenticatedUser.price;
+
+    pool.query("BEGIN;");
+    //jika saldo user < dari price print saldo tidak cukup
+    if (authenticatedUser.balance < price_rent_parking) {
+      res.json("saldo tidak cukup");
+      return "saldo tidak cukup";
+    } else {
+      //jika saldo user > dari price maka kurangi saldo user dengan price
+      await pool.query(
+        `UPDATE parkingUsers SET balance = balance - ${price_rent_parking} WHERE user_id = '${authenticatedUser.user_id}'`
+      );
+
+      //update status booking menjadi paid
+      await pool.query(
+        `UPDATE ordersdetail SET status = 'PAID' WHERE booking_id = '${authenticatedUser.booking_id}'`
+      );
+    }
+
+    //buat testing
+    pool.query("COMMIT;");
+
+    res.json("check-out berhasil");
+    return "check-out berhasil";
+  } catch (err) {
+    pool.query("ROLLBACK;");
+    res.json("check-out gagal");
+    return err;
+  }
+});
+
+//fungsi untuk otentikasi user saat keluar gerbang
+async function authenticateUserOut(plat_number) {
+  try {
+    if (!plat_number) {
+      throw new Error("plat_number is required");
+    }
+    if (typeof plat_number !== "string") {
+      throw new Error("plat_number must be a string");
+    }
+
+    pool.query("BEGIN;");
+
+    //memastikan kalau plat_number user terdaftar di database
+    const result = await pool.query(
+      `SELECT * FROM parkingUsers WHERE plat_number = '${plat_number}'`
+    );
+
+    //error handler jika plat_number tidak terdaftar di database
+    if (!result || !result.rows || !result.rows.length) {
+      return "authenticate user check-out failed";
+    }
+
+    //mengambil data spesifik renting user terhadap sewa parkir
+    const authenticate_user_result = await pool.query(
+      `SELECT * FROM ordersdetail WHERE user_id = '${result.rows[0].user_id}' AND status = 'NOT PAID'`
+    );
+
+    //error handler jika user tidak memiliki data sewa parkir yang belum dibayar
+    if (
+      !authenticate_user_result ||
+      !authenticate_user_result.rows ||
+      !authenticate_user_result.rows.length
+    ) {
+      return "authenticate user check-out failed";
+    }
+
+    //query untuk edit time_exit
+    await pool.query(
+      `UPDATE ordersdetail SET time_exit = NOW() WHERE booking_id = '${authenticate_user_result.rows[0].booking_id}'`
+    );
+
+    //ambil bagian jam saja dari time_exit dan time_enter
+    const hour_time_exit_result = await pool.query(
+      `SELECT EXTRACT(EPOCH FROM time_exit) * 1000 AS time_exit FROM ordersdetail WHERE booking_id = '${authenticate_user_result.rows[0].booking_id}'`
+    );
+    const hour_time_enter_result = await pool.query(
+      `SELECT EXTRACT(EPOCH FROM time_enter) * 1000 AS time_enter FROM ordersdetail WHERE booking_id = '${authenticate_user_result.rows[0].booking_id}'`
+    );
+
+    // query untuk edit price
+    const harga_tiket_parkir = await calculatePrice(
+      hour_time_exit_result.rows[0].time_exit,
+      hour_time_enter_result.rows[0].time_enter
+    );
+
+    //query untuk update harga tiket parkir
+    await pool.query(
+      `UPDATE ordersdetail SET price = ${harga_tiket_parkir} WHERE booking_id = '${authenticate_user_result.rows[0].booking_id}'`
+    );
+
+    //mengumpulkan data agar di tahap selanjutnya lebih mudah di-manipulasi
+    const user_data = {
+      booking_id: authenticate_user_result.rows[0].booking_id,
+      user_id: authenticate_user_result.rows[0].user_id,
+      time_enter: authenticate_user_result.rows[0].time_enter,
+      time_exit: authenticate_user_result.rows[0].time_exit,
+      price: authenticate_user_result.rows[0].price,
+      status: authenticate_user_result.rows[0].status,
+      balance: result.rows[0].balance,
+    };
+
+    pool.query("COMMIT;");
+    return user_data;
+  } catch (err) {
+    pool.query("ROLLBACK;");
+    return err;
+  }
+}
+
+//fungsi untuk otentikasi user saat masuk gerbang berdasarkan plat_number mereka
+async function authenticateUserIn(plat_number) {
+  try {
+    if (!plat_number) {
+      throw new Error("plat_number is required");
+    }
+    if (typeof plat_number !== "string") {
+      throw new Error("plat_number must be a string");
+    }
+
+    //menandakan transaksi dimulai
+    await pool.query("BEGIN;");
+
+    const result = await pool.query(
+      `SELECT * FROM parkingUsers WHERE plat_number = '${plat_number}'`
+    );
+
+    if (!result || !result.rows || !result.rows.length) {
+      return "authenticate user check-in failed";
+    }
+
+    //insert data ke tabel ordersdetail dengan time_enter = NOW()
+    await pool.query(
+      `INSERT INTO ordersdetail (user_id, time_enter, status) VALUES ('${result.rows[0].user_id}', NOW(), 'NOT PAID') RETURNING *`
+    );
+
+    //jika tidak ada error, maka dapat dilakukan commit
+    await pool.query("COMMIT;");
+    return result.rows[0];
+  } catch (err) {
+    //jika ada error, maka transaksi akan dirollback
+    await pool.query("ROLLBACK;");
+    return err;
+  }
+}
+
+async function calculatePrice(time_exit, time_enter) {
+  try {
+    //ini perlu ambil bagian jamnya aja
+
+    //mencari selisih dalam milisecond
+    const diffInMs = Math.abs(time_exit - time_enter);
+
+    //convert selisih dalam milisecond ke dalam jam
+    const diffInHours = Math.ceil(diffInMs / (1000 * 60 * 60));
+
+    //4000 adalah initial harga parkingnya, dan sejam seterusnya akan dikenakan 2000
+    const price = 4000 + (diffInHours - 1) * 2000;
+    return price;
+  } catch (err) {
+    return err.message;
+  }
+}
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
