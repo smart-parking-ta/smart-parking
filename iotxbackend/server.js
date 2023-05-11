c182ea4deda8b4749cfa32f882eef15b6e543683
relieyanhilman <relieyanhilman93@gmail.com>
2023-05-11 16:16:12 +0700

    add simple mqqt function

--- iotxbackend/server.js
+++ iotxbackend/server.js
@@ -180,6 +180,7 @@ app.post(
         time_enter_unixTimeStamp
       );
 
+      // fungsi untuk mengirim data ke mqtt broker sehingga gerbang bisa terbuka
       const mqttClient = req.mqtt;
       mqttClient.publish(
         "backend/checkIn",
@@ -255,6 +256,7 @@ app.post(
         req.booking_data_to_alter.price
       );
 
+      //fungsi untuk mengirim data ke mqtt broker sehingga gerbang bisa terbuka
       const mqttClient = req.mqtt;
       mqttClient.publish(
         "backend/checkOut",
