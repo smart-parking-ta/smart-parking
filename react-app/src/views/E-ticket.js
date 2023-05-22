a79ff392314a541c1c3ef4e2246d4adf82199dab
hakimasyrofi <74890572+hakimasyrofi@users.noreply.github.com>
2023-05-22 11:49:09 +0700

    update e-ticket style

--- react-app/src/views/E-ticket.js
+++ react-app/src/views/E-ticket.js
@@ -10,17 +10,19 @@ function ETicket(){
                 <button onClick={()=>navigate('/dashboard')} className='bg-[#445263] hover:bg-blue-200 hover:text-gray-700 text-white font-medium py-3 px-16 text-xs rounded-full focus:outline-none focus:shadow-outline'>Dashboard</button>
                 <button onClick={()=>navigate('/')} className='bg-[#445263] hover:bg-blue-200 hover:text-gray-700 text-white font-medium py-3 px-16 text-xs rounded-full focus:outline-none focus:shadow-outline'>Log Out</button>
             </div>
-            <div class="flex w-full justify-center p-4">
-            <div class="mx-4 max-w-sm p-10 bg-gradient-to-r g-gradient-to-b from-teal-200 to-blue-50 border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <p class="text-xs font-bold text-gray-700 dark:text-gray-400">Charged</p>
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Rp 5.000</h5>
-                <p class="text-xs font-bold text-gray-700 dark:text-gray-400">In: 2023/05/04 16:40:25, Out: 2023/05/04 17:10:25</p>
-            </div>
-            <div class="mx-4 max-w-sm p-10 bg-gradient-to-r g-gradient-to-b from-teal-200 to-blue-50 border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <p class="text-xs font-bold text-gray-700 dark:text-gray-400">Charged</p>
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Rp 30.000</h5>
-                <p class="text-xs font-bold text-gray-700 dark:text-gray-400">In: 2023/05/04 16:40:25, Out: 2023/05/04 17:10:25</p>
-            </div>
+            <div class="w-full justify-center p-4">
+                <div class="m-4 p-8 bg-gradient-to-r g-gradient-to-b from-teal-200 to-blue-50 border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <p class="text-xs font-bold text-gray-700 dark:text-gray-400">Charged</p>
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Rp 5.000</h5>
+                    <p class="text-xs font-bold text-gray-700 dark:text-gray-400">In: 2023/05/04 16:40:25</p>
+                    <p class="text-xs font-bold text-gray-700 dark:text-gray-400">Out: 2023/05/04 17:10:25</p>
+                </div>
+                <div class="m-4 p-8 bg-gradient-to-r g-gradient-to-b from-teal-200 to-blue-50 border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <p class="text-xs font-bold text-gray-700 dark:text-gray-400">Charged</p>
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Rp 30.000</h5>
+                    <p class="text-xs font-bold text-gray-700 dark:text-gray-400">In: 2023/05/04 16:40:25</p>
+                    <p class="text-xs font-bold text-gray-700 dark:text-gray-400">Out: 2023/05/04 17:10:25</p>
+                </div>
             </div>
             </div>
         </div>
