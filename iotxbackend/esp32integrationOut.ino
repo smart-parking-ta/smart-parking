08c54696be7177c39218a42fa2dd11a77d7f53d2
hakimasyrofi <74890572+hakimasyrofi@users.noreply.github.com>
2023-05-24 21:25:56 +0700

    update add oled

--- iotxbackend/esp32integrationOut.ino
+++ iotxbackend/esp32integrationOut.ino
@@ -8,6 +8,10 @@ extern "C"
 }
 #include <AsyncMqttClient.h>
 #include <Servo.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 #define WIFI_SSID "rel"
 #define WIFI_PASSWORD "gerigiroda"
@@ -17,6 +21,12 @@ extern "C"
 
 #define CLIENT_USERNAME "esp32-checkOut"
 #define CLIENT_PASSWORD "beybladegila123"
+
+#define SCREEN_WIDTH 128 // OLED width,  in pixels
+#define SCREEN_HEIGHT 64 // OLED height, in pixels
+
+// create an OLED display object connected to I2C
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 // Servo Variable
 Servo myservo;
@@ -280,6 +290,21 @@ void setup()
     mqttClient.setServer(MQTT_HOST, MQTT_PORT);
 
     connectToWifi();
+
+      // initialize OLED display with I2C address 0x3C
+    if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+      Serial.println(F("failed to start SSD1306 OLED"));
+      while (1);
+    }
+  
+    delay(2000);         // wait two seconds for initializing
+    oled.clearDisplay(); // clear display
+  
+    oled.setTextSize(1);         // set text size
+    oled.setTextColor(WHITE);    // set text color
+    oled.setCursor(0, 2);       // set position to display (x,y)
+    oled.println("Robotronix"); // set text
+    oled.display();              // display on OLED
 }
 
 void loop()
