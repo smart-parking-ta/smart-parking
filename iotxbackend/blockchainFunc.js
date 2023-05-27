8daf656b7cdc3f21b4e6304d725073764ece8730
hakimasyrofi <74890572+hakimasyrofi@users.noreply.github.com>
2023-05-27 22:05:14 +0700

    add comment

--- iotxbackend/blockchainFunc.js
+++ iotxbackend/blockchainFunc.js
@@ -5,7 +5,8 @@ const web3 = new Web3(
 );
 require("dotenv").config();
 
-const contractAddress = "0x1f2218eE01A96A3d8530fe7D41C14D4F07Fa8358";
+//reset db first
+// const contractAddress = "0x1f2218eE01A96A3d8530fe7D41C14D4F07Fa8358";
 const privateKey = process.env.PRIVATE_KEY;
 web3.eth.accounts.wallet.add(privateKey)
 const accountAddress = web3.eth.accounts.wallet[0].address
